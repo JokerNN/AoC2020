4e4357bf7d4894cd361a26e72668a84af4b6c6d7
ataktaev <andrey.taktaev@booking.com>
2020-12-06 15:48:00 +0100

    7

---
+++ 6.py
@@ -0,0 +1,25 @@
+from functools import reduce
+
+inp = ''
+
+with open('./inp6.txt') as f:
+    inp = f.read()
+
+res = 0
+
+for group in inp.split('\n\n'):
+    lines = (set(l) for l in group.split('\n'))
+    r_set = reduce(lambda i, a: a | i, lines)
+    res += len(r_set)
+
+print("Part 1.", res)
+
+res2 = 0
+
+for group in inp.split('\n\n'):
+    lines = (set(l) for l in group.split('\n'))
+    les = reduce(lambda i, a: a & i, lines)
+    res2 += len(les)
+
+
+print("Part 2.", res2)
