e6bef436f232e667cc08da1cabb8b2c3ce4fdcd8
ataktaev <andrey.taktaev@booking.com>
2020-12-09 12:43:24 +0100

    9

---
+++ 9.py
@@ -0,0 +1,50 @@
+from collections import Counter
+
+inp = ''
+
+with open('./inp9.txt') as f:
+    inp = f.read()
+
+nums = [int(n) for n in inp.split('\n')]
+
+counts = set(nums[:25])
+
+invalid_number = None
+
+for idx, n in enumerate(nums[25:], 25):
+    found_pair = False
+
+    for k in counts:
+        if n - k in counts:
+            found_pair = True
+            break
+
+    if not found_pair:
+        invalid_number = n
+        break
+
+    counts.discard(nums[idx - 25])
+    counts.add(n)
+
+
+print('Part 1.', invalid_number)
+
+ps = 0
+pe = 0
+s = nums[0]
+
+while s != invalid_number:
+    if s < invalid_number:
+        pe += 1
+        s += nums[pe]
+    elif s > invalid_number:
+        s -= nums[ps]
+        ps += 1
+
+    # if s == invalid_number:
+    #     break
+
+sumset = nums[ps:pe + 1]
+
+print('Part 2.', min(sumset) + max(sumset))
+        
